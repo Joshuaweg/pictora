6a84005bfcf010d30844476b68390df748164986
BellMatt38 <115511313+BellMatt38@users.noreply.github.com>
2024-10-21 18:26:51 -0700

    Added the start of the generator

--- Pictora/ImageGeneratePage.xaml.cs
+++ Pictora/ImageGeneratePage.xaml.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotNetEnv;
 
 namespace Pictora
 {
     public partial class ImageGeneratePage : ContentPage
     {
+        string API_KEY = "";
         public ImageGeneratePage()
         {
-
             InitializeComponent();
-    
-
+            API_KEY = Env.GetString("FAL_API_KEY");
         }
     }
     
