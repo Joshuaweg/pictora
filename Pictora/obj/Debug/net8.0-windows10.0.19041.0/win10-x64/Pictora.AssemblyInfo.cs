e05241ab1660d8090fd920564786ed6a95741a80
Joshua Weg <Joshua.weg.95@gmail.com>
2024-11-25 08:54:33 -0800

    merging

--- Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
+++ Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6f352bd861bd65f10141b25e7a9553a30aa0897c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+34c630c4af71816991dcfecdc8d47b46ccaf2628")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
