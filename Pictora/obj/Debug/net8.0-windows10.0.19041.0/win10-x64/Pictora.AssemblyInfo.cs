935de2d5dbe9a5e5cf02b7913e8e68f0b86f67bc
BellMatt38 <115511313+BellMatt38@users.noreply.github.com>
2024-11-24 21:22:02 -0800

    Added color to the Gallery Button.

--- Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
+++ Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+223db2a787bc4a1aaf60edc8feee867dcf5133bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bb3f469511ebcf05a565ce80c29e7b156e831047")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
