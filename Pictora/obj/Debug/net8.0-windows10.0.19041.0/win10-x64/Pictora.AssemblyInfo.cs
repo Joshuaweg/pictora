6928bfb0b554d066f315513a11f4dec6f8b20718
jscodes2021 <joshsant2021@gmail.com>
2024-11-24 22:06:36 -0800

    Revert "Merge of outputs and caches"
    This reverts commit 5ab6480f6a66969b93e86292b6306a49395e5677.

--- Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
+++ Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d5c2b4794d547058ad70bb386e4aa54c8f46773c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+392500561c72ac1d1be26aba9567226cf813cece")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
