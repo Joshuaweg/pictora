befb4fb7ee2d24773059ec19a0f5b4677b96203f
jscodes2021 <joshsant2021@gmail.com>
2024-11-24 23:35:53 -0800

    Minor UI tweaks/updates
    Added icons to entry fields and changed placeholder color to a more vivid white color.

--- Pictora/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Pictora.AssemblyInfo.cs
+++ Pictora/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Pictora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6928bfb0b554d066f315513a11f4dec6f8b20718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+74ff50886d6bc866547018f81a0d2c18b0b70f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
