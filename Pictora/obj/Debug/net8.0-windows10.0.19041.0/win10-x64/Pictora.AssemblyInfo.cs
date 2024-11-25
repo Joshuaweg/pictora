56ddbe924dbef0c12349a03d9363a85dabdfb65d
jscodes2021 <joshsant2021@gmail.com>
2024-11-25 13:02:49 -0800

    More Minor UI Changes
    Uniformized Font Styles for the Image Editor page and added some margins to create separation in between buttons. Additionally, fixed the padding and margins in the ImageGeneratePage; made logo circular.

--- Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
+++ Pictora/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Pictora.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+74ff50886d6bc866547018f81a0d2c18b0b70f05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+befb4fb7ee2d24773059ec19a0f5b4677b96203f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pictora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
