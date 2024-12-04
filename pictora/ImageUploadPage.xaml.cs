ec538bed1e5c7205befba916da2690b5c4793a1e
Joshua Weg <Joshua.weg.95@gmail.com>
2024-12-03 18:19:08 -0800

    merging with Gallery and Upload changes

--- pictora/ImageUploadPage.xaml.cs
+++ pictora/ImageUploadPage.xaml.cs
@@ -22,7 +22,7 @@ public partial class ImageUploadPage : ContentPage
 {
     Pictora.Models.Image generated_image;
     string edited_image; // Parameter, need to be converted to a url either by code or uploading to a private service.
-	public ImageUploadPage(Result image)
+	public ImageUploadPage(Result image, int idx =0)
 	{
         InitializeComponent();
 
@@ -36,7 +36,7 @@ public partial class ImageUploadPage : ContentPage
         generated_image.Model = image.model;
         generated_image.Style = image.style;
         generated_image.Tags = new List<string>();  // Defined on save page
-        generated_image.UserId = 0;                // Defined on login page
+        generated_image.UserId = idx;                // Defined on login page
         generated_image.Upvotes = 0;
         generated_image.Downvotes = 0;
         generated_image.Description = "";            // Defined on save page
@@ -82,7 +82,6 @@ public partial class ImageUploadPage : ContentPage
             {
                 await DisplayAlert("Notice", "I still need to convert the edited file to a url. \n\n Nothing has been uploaded", "Ok");
             });
-            return;
         }
 
         Debug.Print("Upload should be fine if it gets to this point.");
@@ -93,10 +92,10 @@ public partial class ImageUploadPage : ContentPage
         // Go through the image database, and find the next unsued NumericId.
 
         // Do not uncoment this until both of the unset fields have been set properly.
-        /*
+       
         MongoDBService mgdbs = new();
         mgdbs.CreateAsync("images", generated_image);
-        */
+        
 
         // Display a prompt, to confirm the image has been uploaded sucessfully
         // Switch view back to home page, because otherwise the user may spam the database with duplcates
